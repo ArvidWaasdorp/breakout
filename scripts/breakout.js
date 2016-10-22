c7d01ce4297d21815ec64ed5241be5e93df16668
Arvid <a.waasdorp@gmail.com>
2016-10-22 13:43:22 +0200

    Update classes. fixed movement, broke it again :)

--- scripts/breakout.js
+++ scripts/breakout.js
@@ -1,27 +1,28 @@
-// //TODO
+//TODO
 
-// //Create bat                  done   .
-// //Move bat using keyboard     done   .
-// //Create ball                 done   .
-// //Move ball                   done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
-// //Let ball bounch             done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
-// //Create collision on wall    done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
-// //Create collision on bat     open
-// //Create blocks               open
-// //Remove blocks               open
-// //Create collision on blocks  open
-// //Create score                open
-// //Create menu                 open
-// //Create levels               open
+//Create bat                  done   .
+//Move bat using keyboard     done   .
+//Create ball                 done   .
+//Move ball                   done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
+//Let ball bounch             done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
+//Create collision on wall    done   https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
+//Make use of object          done
+//Create collision on bat     open
+//Create blocks               open
+//Remove blocks               open
+//Create collision on blocks  open
+//Create score                open
+//Create menu                 open
+//Create levels               open
 
 
 // http://stackoverflow.com/questions/11368477/dynamically-resize-canvas-window-with-javascript-jquery
-// ? https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 // https://scotch.io/tutorials/default-sizes-for-twitter-bootstraps-media-queries
 
-// 
-//   var boxes = [];
+//Help for collision detection 
+// ? https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 
+//   var boxes = [];
 //   //Box object to hold data for all drawn rects
 //   function Box() {
 //     this.x = 0;
@@ -35,69 +36,43 @@
 
 $( document ).ready(function() {
 
-  //768, 800*600, ratio: 0.75 (small devices, tablets)
-  //var canvas_width = 640;
-  //var canvas_height = 480;
-  
-  //992 medium devices > resolution: 800*600
-  //var canvas_width = 768;
-  //var canvas_height = 576;
-
-
   var canvas  = document.getElementById('gameCanvas');
   var ctx     = canvas.getContext('2d');
 
   var ballRadius  = 10;
-  var x           = canvas.width/2;
-  var y           = canvas.height-30;
+  var ballX       = canvas.width/2;
+  var ballY       = canvas.height-60;
   var dx          = 2;
   var dy          = -2;
-  var batX        = 100;
-  var batY        = 550;
-  var batW        = 100;
-  var key         = '';
+
+  var batW        = 100;                          //Width of the bat
+  var batY        = 550;                          //Pos Y of the bat. Always 550px in this case;
+  var batX        = (canvas.width/2) - (batW/2);  //Pos X of the bat, starting point. Center of the canvas
+  var batS        = 5;                            //Speed of the bat movement
 
   var bat         = null;
   var ball        = null;
 
+  //Init the game. Set default values
   init();
 
-  $(document).keydown(function(e){
+  //Run the game
+  console.log('Running game');
 
-    switch(e.keyCode || e.which) {
-      case 37:                //left
-        key = 'left';
-        batX = batX - 10;
-        break;
-      case 38:                //up
-        key = 'up';
-        bat.changeWidth(200);
-        break;
-      case 39:                //right
-        key = 'right';
-        batX = batX + 10;
-        break;
-      case 40:                //down
-        key = 'down';
-        bat.changeWidth(100);
-        break;
-      case 27:                //escape
-        key = 'Escape';
-        break;
-    }
 
-    if (batX < 0) batX = 0;
-    if (batX > (canvas.width-bat.w)) batX = (canvas.width-bat.w);
-  });
+  //The game loop
+  setInterval(run, 10);
 
+
+  //Functions we can use
+  //Draw some debug information
   function drawDebug() {
     $('#data-bat-x').text(batX);
     $('#data-bat-y').text(batY);
-    $('#data-ball-x').text(x);
-    $('#data-ball-y').text(y);
+    $('#data-ball-x').text(ballX);
+    $('#data-ball-y').text(ballY);
     $('#key').text(key);
   }
-
 
   function init() {
 
@@ -107,65 +82,119 @@ $( document ).ready(function() {
     if (!bat) {
       console.log('ERROR: Unable to create bat object');
     }
-    bat.setDimension(batX, batY, batW, 'black', 'yellow');
+    bat.init(batX, batY, batW, batS, 'black', 'white');
 
     ball = new objectBall();
     if (!ball) {
       console.log('ERROR: Unable to create ball object');
     }
-    ball.setDimension(x, y, ballRadius, '#0095DD');
+    ball.init(ballX, ballY, ballRadius, '#0095DD', dx, dy);
+    ball.changeXSpeed(dx);
+    ball.changeYSpeed(dy);
+   
+  }
 
+
+  function run() {
+    collision();
+
+    bat.changePosition(batX, batY);
+    ball.changePosition(ballX, ballY);
+
+    getInput();
+
+    draw();
   }
 
   function draw() {
+
+    //Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-
-    //drawBall();
-
-    ball.drawIt(x, y);
-    bat.drawIt(batX, batY);
-
-
-    collision();
-
+    ball.draw();
+    bat.draw();
 
     if ($('#debug').is(':checked') === true) {
       drawDebug();
     }
+
   }
 
+  function getInput() {
+
+    window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
+    window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+
+    if (Key.isDown(Key.UP))    batS = bat.changeSpeed(1.1);  //this.moveUp();
+    if (Key.isDown(Key.LEFT))  bat.changePosition(bat.batX - bat.batS, bat.batY);   //this.moveLeft();
+    if (Key.isDown(Key.DOWN))  bat.changeWidth(200);                                //this.moveDown();
+    if (Key.isDown(Key.RIGHT)) bat.changePosition(bat.batX + bat.batS, bat.batY);   //this.moveRight();
+  }
+
+
   function collision() {
+
+    //Make sure the bat is not going out of the canvas
+    if (batX < 0)                     batX = 0;
+    if (batX > (canvas.width-bat.w))  batX = (canvas.width-bat.w);
+
     //Doe stuiter-shit met the ball
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if(ballX + dx > canvas.width-ballRadius || ballX + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(ballY + dy > canvas.height-ballRadius || ballY + dy < ballRadius) {
         dy = -dy;
     }
-
 
     // if (distance < circle1.radius + circle2.radius) {
     // // collision detected!
     // }
     
-    x += dx;
-    y += dy;
+    ballX += dx;
+    ballY += dy;
 
   }
 
-
   //Object Ball
-  function objectBall(x, y, r, color){
+  function objectBall(x, y, r, color, sX, sY){
     this.x = x;
     this.y = y;
     this.r = r;
     this.color = color
+    this.sX = sX;
+    this.sY = sY;
 
-    objectBall.prototype.drawIt = function(x, y) {
+    objectBall.prototype.init = function(x, y, r, color, sX, sY) {
       this.x = x;
       this.y = y;
+      this.r = r;
+      this.color = color;
+      this.sX = sX;
+      this.sY = sY;
+    };
 
+    objectBall.prototype.changePosition = function(x, y) {
+      this.x = x;
+      this.y = y;
+    };
+
+    objectBall.prototype.changeRadius = function(r) {
+      this.r = r;
+    };
+
+    objectBall.prototype.changeColor = function(color) {
+      this.color = color;
+    };
+
+    objectBall.prototype.changeXSpeed = function(sX) {
+      this.sX = sX;
+    };
+
+    objectBall.prototype.changeYSpeed = function(sY) {
+      this.sY = sY;
+    };
+
+    objectBall.prototype.draw = function() {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
       ctx.fillStyle = this.color; //'#0095DD';
@@ -173,26 +202,47 @@ $( document ).ready(function() {
       ctx.closePath();
     };
 
-    objectBall.prototype.setDimension = function (x, y, r, color) {
-      this.x = x;
-      this.y = y;
-      this.r = r;
-      this.color = color;
-    };
+
   };
 
   //Object Bat
-  function objectBat(x, y, w, borderColor, fillColor){
+  function objectBat(x, y, w, s, borderColor, fillColor){
     this.x = x;
     this.y = y;
     this.w = w;
+    this.s = s;
     this.border = borderColor;
     this.fill = fillColor;
 
-    objectBat.prototype.drawIt = function(x, y) {
+    objectBat.prototype.init = function(x, y, w, s, borderColor, fillColor) {
       this.x = x;
       this.y = y;
+      this.w = w;
+      this.s = s;
+      this.border = borderColor;
+      this.fill = fillColor;
+    };
 
+    objectBat.prototype.changePosition = function(x, y) {
+      this.x = x;
+      this.y = y;
+    };
+
+    objectBat.prototype.changeWidth = function(w) {
+      this.w = w;
+    };
+
+    objectBat.prototype.changeColor = function(borderColor, fillColor) {
+      this.border = borderColor;
+      this.fill = fillColor;
+    };
+
+    objectBat.prototype.changeSpeed = function(multiplierS) {
+      this.s = this.s*multiplierS;
+      return this.s;
+    };
+
+    objectBat.prototype.draw = function() {
       ctx.beginPath();
       ctx.rect(this.x, this.y, this.w, 20);
       ctx.fillStyle = this.fill;
@@ -201,21 +251,29 @@ $( document ).ready(function() {
       ctx.strokeStyle = this.border;
       ctx.stroke();
     };
-
-    objectBat.prototype.setDimension = function (x, y, w, borderColor, fillColor) {
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.border = borderColor;
-      this.fill = fillColor;
-    };
-
-    objectBat.prototype.changeWidth = function(w) {
-      this.w = w;
-    }
-
+   
   };
 
-  setInterval(draw, 10);
+  //Create key-object
+  var Key = {
+    _pressed: {},
+
+    LEFT:   37,
+    UP:     38,
+    RIGHT:  39,
+    DOWN:   40,
+    
+    isDown: function(keyCode) {
+      return this._pressed[keyCode];
+    },
+    
+    onKeydown: function(event) {
+      this._pressed[event.keyCode] = true;
+    },
+    
+    onKeyup: function(event) {
+      delete this._pressed[event.keyCode];
+    }
+  };
 
 });
